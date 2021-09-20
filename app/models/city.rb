412f74b1e8d486ae1bc8ac0503e2bd601f95f794
= <rtyenriques@gmail.com>
2021-09-20 13:42:11 -0700

    commit tree 97de095ffb7c5bf8c01855b856b69ad023e25d00

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,5 +2,21 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
+  def city_openings(date1, date2)
+    city = City.all
+    city.all.each do |c|
+      if c.listings == date1..date2
+        c.listings.each do |l|
+          l
+        end
+      else
+      end
+
+
+    end
+  
+
+  end
+
 end
 
